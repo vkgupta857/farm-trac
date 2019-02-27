4f9013427a714f6c28c78103babbcd49fbca12dd
Vinod Kumar Gupta <vkgupta857@gmail.com>
2019-02-27 15:02:59 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<footer>
+    <div class="container">
+        <div class="row">            
+            <div class="col-md-6">
+                <h4> Farm Trac - Track Your Food</h4>
+                <p>                            
+                    Address: Gokalpur, Jabalpur 482011<br />
+                    Contact: +91-9479475120<br/>
+                    Email: support@farmtrac.co.in<br/>
+                </p>
+            </div>             
+            <div class="col-md-3 col-xs-6">
+                <div class="footer-list">
+                    <h4>About</h4>
+                    <div class='footer-list-item'>
+                        <a href ="about.php">About Us</a>
+                    </div>
+                    <div class='footer-list-item'>
+                        <a href ="contact.php">Contact Us</a>
+                    </div>
+                    <div class='footer-list-item'>
+                        <a href ="#">Privacy Policy</a>
+                    </div>
+                    <div class='footer-list-item'>
+                        <a href ="#">Terms of Use</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6">
+                <h4>Follow Us</h4>
+                <a href="#"><button class="btn btn-light"><i class="fa fa-google-plus fa-lg"></i></button></a>
+                <a href="#"><button class="btn btn-light"><i class="fa fa-facebook fa-lg"></i></button></a>
+                <a href="#"><button class="btn btn-light"><i class="fa fa-twitter fa-lg"></i></button></a>
+                <a href="#"><button class="btn btn-light"><i class="fa fa-instagram fa-lg"></i></button></a> 
+            </div> 
+        </div><hr />
+        <div class="row">
+            <div class="col-xs-12">
+                &copy; <?php echo date('Y');?> <a href="#"> Farm Trac</a>. All Rights Reserved.
+            </div>   
+        </div>
+    </div>
+</footer>
