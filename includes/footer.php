2f815c328c9ba98267639f5aa2c386a8e97b2389
Vinod Kumar Gupta <vkgupta857@gmail.com>
2019-02-27 14:43:41 +0530

    footer added

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<footer>
+    <div class="container">
+        <div class="row">            
+            <div class="col-md-6">
+                <h3 style="color: #e52d27;"> NoBag</h3>
+                <p>                            
+                    Address: Gokalpur, Jabalpur 482011<br />
+                    Contact: <a href="tel:+919926131113">+91-9926131113</a><br/>
+                    Email: <a href="mailto:support@nobag.co.in">support@nobag.co.in</a><br/>
+                </p>
+            </div>             
+            <div class="col-md-3 col-xs-6">
+                <div class="footer-list">
+                    <h4>About</h4>
+                    <div class='footer-list-item'>
+                        <a href ="http://nobag.co.in/about.php">About Us</a>
+                    </div>
+                    <div class='footer-list-item'>
+                        <a href ="http://nobag.co.in/contact.php">Contact Us</a>
+                    </div>
+                    <div class='footer-list-item'>
+                        <a href ="http://nobag.co.in/privacy.php">Privacy Policy</a>
+                    </div>
+                    <div class='footer-list-item'>
+                        <a href ="http://nobag.co.in/terms.php">Terms of Use</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6">
+                <h4>Follow Us</h4>
+                <a href="#"><button class="btn btn-light"><i class="fa fa-google-plus fa-lg"></i></button></a>
+                <a href="#"><button class="btn btn-light"><i class="fa fa-facebook fa-lg"></i></button></a>
+                <a href="#"><button class="btn btn-light"><i class="fa fa-twitter fa-lg"></i></button></a>
+                <a href="#"><button class="btn btn-light"><i class="fa fa-instagram fa-lg"></i></button></a> 
+            </div> 
+        </div><hr />
+        <div class="row">
+            <div class="col-xs-12">
+                &copy; <?php echo date('Y');?> <a href="#"> Nobag</a>. All Rights Reserved.
+            </div>   
+        </div>
+    </div>
+</footer>
