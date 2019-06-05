99724d725b0adf9d29a4328ed3d34e1e446f17e1
vkgupta857 <vinod.kumar@cognerainc.com>
2019-06-05 16:51:45 +0530

    Modified brand name in index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['farmer'])){
 <!DOCTYPE html>
 <html lang="en">
     <head>        
-        <title>Trac - Know your food</title>
+        <title>Farm Trac - Know your food</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
